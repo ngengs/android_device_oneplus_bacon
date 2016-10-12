2ff4d1f5dcd3c869c14d32ce94dbf53812cca56d
Martinusbe <martinusbe@gmail.com>
2016-10-12 08:39:45 +0200

    bacon: enable keystore
    Signed-off-by: Martinusbe <martinusbe@gmail.com>

--- bacon.mk
+++ bacon.mk
@@ -116,8 +116,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
 
 # Keystore
-#PRODUCT_PACKAGES += \
-#    keystore.msm8974
+PRODUCT_PACKAGES += \
+    keystore.msm8974
 
 # Lights
 PRODUCT_PACKAGES += \
